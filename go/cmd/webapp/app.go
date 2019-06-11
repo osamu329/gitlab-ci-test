b126f1587451255edbacc9efffebd15492fe51bf
Osamu NAKAMURA <osamu0329nakamura@gmail.com>
2019-06-11 19:35:41 +0900

    change port to 8080

--- go/cmd/webapp/app.go
+++ go/cmd/webapp/app.go
@@ -12,5 +12,5 @@ func hello(w http.ResponseWriter, rqst *http.Request) {
 
 func main() {
 	http.HandleFunc("/", hello)
-	log.Fatal(http.ListenAndServe("0.0.0.0:8000", nil))
+	log.Fatal(http.ListenAndServe("0.0.0.0:8080", nil))
 }
